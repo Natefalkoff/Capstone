7460dd830ade4ae364e2940cbf177e6701335682
Nathan Falkoff <natefalkoff@gmail.com>
2017-11-17 16:08:07 -0500

    4:08

---
+++ Capstone.Web/Models/PlanModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class PlanModel
+    {
+        public int PlanID { get; set; }
+        public string DayOfWeek { get; set; }
+        public List<RecipeModel> Recipes { get; set; }
+    }
+}
---
+++ Capstone.Web/Models/RecipeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class RecipeModel
+    {
+        public int RecipeID { get; set; }
+        public string Name { get; set; }
+        public string Directions { get; set; }
+        public int UserID { get; set; }
+        public string Ingredients { get; set; }
+        public string ImageName { get; set; }
+        public List<string> Tags { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
