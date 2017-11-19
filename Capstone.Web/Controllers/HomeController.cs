980797d8e96f99b6b4cf2135f40424265621b16f
Nathan Falkoff <natefalkoff@gmail.com>
2017-11-19 10:14:20 -0500

    sunday merge

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Capstone.Web.DAL;
+using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,11 +11,43 @@ namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["recipeDB"].ConnectionString;
+
+      
 
         // GET: Home
         public ActionResult Index()
         {
+          
             return View("Index");
         }
+
+        
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register", model);
+            }
+
+            UserSqlDAL userDal = new UserSqlDAL(connectionString);
+            userDal.RegisterUser(model);
+
+            return RedirectToAction("RegisterSuccess", "Home");
+        }
+
+        public ActionResult RegisterSuccess()
+        {
+            return View();
+        }
+
+
     }
 }
--- Capstone.Web/Models/UserModel.cs
+++ Capstone.Web/Models/UserModel.cs
@@ -5,12 +5,15 @@ using System.Web;
 
 namespace Capstone.Web.Models
 {
-    public class UserModelcs
+    public class UserModel
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string ConfirmEmail { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int AuthorizationLevel = 1;
         public List<RecipeModel> Recipes { get; set; }
         public List<RecipeModel> Plan { get; set; }
     }
