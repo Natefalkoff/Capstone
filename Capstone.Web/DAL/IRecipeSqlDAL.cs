6e8a86be519fe049a11e2e8671d4845849a08766
Rob Rankin <rob.rankin.jr@gmail.com>
2017-11-20 10:05:43 -0500

    more add recipe stuff

--- Capstone.Web/DAL/IRecipeSqlDAL.cs
+++ Capstone.Web/DAL/IRecipeSqlDAL.cs
@@ -12,5 +12,9 @@ namespace Capstone.Web.DAL
        int NewRecipe(RecipeModel recipe);
        List<RecipeModel> GetRecipes();
        RecipeModel RecipeDetail(int id);
+        bool InsertRecipeIdAndTagId(int recipeId, int tagId);
+        int GetTagIdAfterInsert(string tag);
+        int GetTagIdIfExists(string tag);
+        List<int> TagsExist(string tags);
     }
 }
