c59e41d1e9ca74ec7d3b8a156eae72e9b27bad47
Rob Rankin <rob.rankin.jr@gmail.com>
2017-11-22 22:19:02 -0500

    fixed search and registration

--- Capstone.Web/DAL/SearchSqlDAL.cs
+++ Capstone.Web/DAL/SearchSqlDAL.cs
@@ -42,13 +42,13 @@ namespace Capstone.Web.DAL
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-                    if(list.Count > 0)
+                    if(list.Count > 0 && model.TagSearch == null)
                     {
                         for(int i = 0; i < list.Count; i++)
                         {
                             conn.Open();
                             
-                            string categoryResults = string.Format(@"SELECT * FROM recipe JOIN recipe_category ON recipe.recipe_id = recipe_category.recipe_id JOIN category ON recipe_category.category_id = category.category_id WHERE category_name = @catName{0};", i);
+                            string categoryResults = string.Format(@"SELECT * FROM recipe JOIN recipe_category ON recipe.recipe_id = recipe_category.recipe_id JOIN category ON recipe_category.category_id = category.category_id WHERE category_name = @catName{0} AND approved = 1 AND publics = 1;", i);
                             SqlCommand cmd = new SqlCommand(categoryResults, conn);
                             cmd.Parameters.AddWithValue(string.Format("@catName{0}", i), list[i]);
                             SqlDataReader read = cmd.ExecuteReader();
@@ -66,7 +66,7 @@ namespace Capstone.Web.DAL
                         }
                     }
                     
-                    if (model.TagSearch != null)
+                    if (tagStrings.Count > 0 && list.Count == 0)
                     {
                         if (tagStrings.Count > 0)
                         {
@@ -74,7 +74,7 @@ namespace Capstone.Web.DAL
                             {
                                 conn.Open();
                                 
-                                string tagResults = string.Format(@"SELECT * FROM recipe JOIN recipe_tags ON recipe.recipe_id = recipe_tags.recipe_id JOIN tags ON recipe_tags.tag_id = tags.tag_id WHERE tag_name LIKE @tagName{0};", i);
+                                string tagResults = string.Format(@"SELECT * FROM recipe JOIN recipe_tags ON recipe.recipe_id = recipe_tags.recipe_id JOIN tags ON recipe_tags.tag_id = tags.tag_id WHERE tag_name = @tagName{0} AND approved = 1 AND publics = 1;", i);
                                 SqlCommand cmd = new SqlCommand(tagResults, conn);
                                 cmd.Parameters.AddWithValue(string.Format("@tagName{0}", i), tagStrings[i]);
                                 SqlDataReader read = cmd.ExecuteReader();
@@ -89,6 +89,33 @@ namespace Capstone.Web.DAL
                                     results.Add(r);
                                 }
                                 conn.Close();
+                            }
+                        }
+                        if(tagStrings.Count > 0 && list.Count > 0)
+                        {
+                            for (int i = 0; i < tagStrings.Count; i++)
+                            {
+                                for (int j = 0; j < list.Count; j++)
+                                {
+                                    conn.Open();
+
+                                    string tagResults = string.Format(@"SELECT * FROM recipe JOIN recipe_tags ON recipe.recipe_id = recipe_tags.recipe_id JOIN tags ON recipe_tags.tag_id = tags.tag_id WHERE tag_name = @tagName{0} AND category_name = @catName{0} AND approved = 1 AND publics = 1;", i);
+                                    SqlCommand cmd = new SqlCommand(tagResults, conn);
+                                    cmd.Parameters.AddWithValue(string.Format("@tagName{0}", i), tagStrings[i]);
+                                    cmd.Parameters.AddWithValue(string.Format("@catName{0}", j), list[j]);
+                                    SqlDataReader read = cmd.ExecuteReader();
+                                    while (read.Read())
+                                    {
+                                        RecipeModel r = new RecipeModel();
+                                        r.Name = Convert.ToString(read["recipe_name"]);
+                                        r.Directions = Convert.ToString(read["directions"]);
+                                        r.ImageName = Convert.ToString(read["image_name"]);
+                                        r.Ingredients = Convert.ToString(read["ingredients"]);
+                                        r.RecipeID = Convert.ToInt32(read["recipe_id"]);
+                                        results.Add(r);
+                                    }
+                                    conn.Close();
+                                }
                             }
                         }
                     }
